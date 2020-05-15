8210aac0b4b95ccbd9d2b0661b4c0650bf2b69f5
Ismaila Hassan <ismailah28@gmail.com>
2020-05-15 18:28:51 +0100

    chore: setup environment variable

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import { welcomeMessage } from '../settings';
 
 const indexRouter = express.Router();
 
 /* GET home page. */
 indexRouter.get('/', (req, res, next) => {
-  res.status(200).json({ message: 'Welcome to Teamwork API!' });
+  res.status(200).json({ message: welcomeMessage });
 });
 
 export default indexRouter;
